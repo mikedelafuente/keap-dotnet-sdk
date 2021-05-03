f14dfd6a67148ab759718f45a25fdbc62b9ec1c2
Weekend Project <weekendprojectapp@gmail.com>
2021-05-03 08:47:01 -0700

    Wire up to Locale endpoints for getting countries and provinces

---
+++ src/KeapSdk/Keap.Sdk/Clients/Locale/CountriesDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Keap.Sdk.Clients.Locale
+{
+    internal class CountriesDto
+    {
+        [JsonPropertyName("countries")]
+        public Dictionary<string, string> Countries { get; set; }
+    }
+}
